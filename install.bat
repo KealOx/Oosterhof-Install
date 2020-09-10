895f33750f2dafec2815f62195f6e16442860db2
Alec Siemerink <41921577+KealOx@users.noreply.github.com>
2020-09-10 18:51:37 +0200

    Check if LibreOffice is needed
    Added a yes/no check for the tech to either install with or without LibreOffice.

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
- 
+
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
@@ -8,32 +8,46 @@ REM  --> Check for permissions
 ) ELSE (
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
- 
+
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
 ) else ( goto gotAdmin )
- 
+
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params= %*
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
- 
+
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
- 
+
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
- 
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
- 
+
 choco feature enable -n allowGlobalConfirmation
-choco install anydesk -A adobereader googlechrome vlc libreoffice-fresh winrar cdrtfe
- 
+
+Choice /M "Wil je LibreOffice?"
+If Errorlevel 2 Goto No
+
+If Errorlevel 1 Goto Yes
+
+Goto End
+:No
+Echo Zonder LibreOffice Af
+choco install anydesk -A adobereader googlechrome vlc winrar cdrtfe
 bitsadmin.exe /transfer "WinRAR" https://oosterhofhulp.nl/rarreg.key "%ProgramFiles%\WinRar\rarreg.key"
- 
+Goto End
+:Yes
+Echo LibreOffice Af
+choco install anydesk -A adobereader googlechrome vlc libreoffice-fresh winrar cdrtfe
+bitsadmin.exe /transfer "WinRAR" https://oosterhofhulp.nl/rarreg.key "%ProgramFiles%\WinRar\rarreg.key"
+:End
+
 PAUSE
